b8c579de401775e0a65c38033714dc633d4c35f5
AbhishekKP-10 <60152693+AbhishekKP-10@users.noreply.github.com>
2022-06-13 23:16:32 +0530

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo `date`
-
-cpu_threshold='0'
+read $output
+cpu_threshold='80'
  
 mem_threshold='80'
  
@@ -12,7 +12,8 @@ cpu_idle=`top -b -n 1 | grep Cpu | awk '{print $8}'|cut -f 1 -d "."`
 cpu_use=`expr 100 - $cpu_idle`
 if [ $cpu_use -gt $cpu_threshold ]
    then
-        curl -X POST -H 'Content-type: application/json' --data '{"text":"cpu warning!!! more than threshold(80%)"}' https://hooks.slack.com/services/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
+        curl -X POST -H 'Content-type: application/json' --data '{"text":"cPU wArNiNg..."}' https://hooks.slack.com/services/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
+        #curl -X POST -H 'Content-type: application/json' --data '{"text":"cpu warning!!! more than threshold(80%)"}' https://hooks.slack.com/services/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
 fi
 }
 
@@ -20,23 +21,21 @@ mem_usage () {
 mem_free=`free -m | grep "Mem" | awk '{print $4+$6}'`
 if [ $mem_free -lt $mem_threshold  ]
     then
-        curl -X POST -H 'Content-type: application/json' --data '{"text":"mem warning!!! more than threshold(80%)"}' https://hooks.slack.com/services/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
+        curl -X POST -H 'Content-type: application/json' --data '{"mem -alert!"}' https://hooks.slack.com/services/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
+        #curl -X POST -H 'Content-type: application/json' --data '{"text":"mem warning!!! more than threshold(80%)"}' https://hooks.slack.com/services/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
 fi
 }
 
 disk_usage () {
-df -Ph | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5,$1 }' | while read output;
-do
-  echo $output
-  used=$(echo $output | awk '{print $1}' | sed s/%//g)
-  partition=$(echo $output | awk '{print $2}')
-  if [ $used -ge 60 ]; 
-  then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"Disk Space Alert more than threshold(80%)"}' https://hooks.slack.com/services/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
-  fi
-done
+CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+if [ "$CURRENT" -gt "$disk_threshold" ]
+then 
+  
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"Disk-Alert"}' https://hooks.slack.com/services/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
+fi
 }
 cpu_usage
 mem_usage
 disk_usage
+
 
