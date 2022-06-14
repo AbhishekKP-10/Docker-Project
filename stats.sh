aed9d42438aa73f0a2386775c9da10e395b04ca7
AbhishekKP-10 <60152693+AbhishekKP-10@users.noreply.github.com>
2022-06-14 12:42:37 +0530

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -12,7 +12,8 @@ cpu_idle=`top -b -n 1 | grep Cpu | awk '{print $8}'|cut -f 1 -d "."`
 cpu_use=`expr 100 - $cpu_idle`
 if [ $cpu_use -gt $cpu_threshold ]
    then
-        curl -X POST -H 'Content-type: application/json' --data '{"text":"cPU wArNiNg..."}' https://hooks.slack.com/services/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
+        curl -X POST -H 'Content-type: application/json' --data '{"text":"CpU WaRnInG.."}' https://hooks.slack.com/services/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
+        #curl -X POST -H 'Content-type: application/json' --data '{"text":"cPU wArNiNg..."}' https://hooks.slack.com/services/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
         #curl -X POST -H 'Content-type: application/json' --data '{"text":"cpu warning!!! more than threshold(80%)"}' https://hooks.slack.com/services/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
 fi
 }
@@ -21,17 +22,18 @@ mem_usage () {
 mem_free=`free -m | grep "Mem" | awk '{print $4+$6}'`
 if [ $mem_free -lt $mem_threshold  ]
     then
-        curl -X POST -H 'Content-type: application/json' --data '{"mem -alert!"}' https://hooks.slack.com/services/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
+        curl -X POST -H 'Content-type: application/json' --data '{"text":"mem WaRnInG..."}' https://hooks.slack.com/services/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
+        #curl -X POST -H 'Content-type: application/json' --data '{"mem -alert!"}' https://hooks.slack.com/services/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
         #curl -X POST -H 'Content-type: application/json' --data '{"text":"mem warning!!! more than threshold(80%)"}' https://hooks.slack.com/services/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
 fi
 }
 
 disk_usage () {
-CURRENT=$(df / | grep / | awk '{ print $5}' | sed 's/%//g')
+CURRENT=`df / | grep / | awk '{ print $5}' | sed 's/%//g'`
 if [ "$CURRENT" -gt "$disk_threshold" ]
 then 
-  
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"Disk-Alert"}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"Disk WaRnInG"}' https://hooks.slack.com/services/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
+    #curl -X POST -H 'Content-type: application/json' --data '{"text":"Disk-Alert"}' https://hooks.slack.com/services/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
 fi
 }
 cpu_usage
